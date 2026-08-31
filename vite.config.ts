e4577eb2ea8843cc2bfbd794ade84a57e6bbf6fa
ciz97-cmyk <ciz97@seznam.cz>
2026-08-31 19:33:51 +0200

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
+
+export default defineConfig(() => {
+  return {
+    base: './',
+    plugins: [react(), tailwindcss(), viteSingleFile()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+    build: {
+      cssCodeSplit: false,
+      assetsInlineLimit: 100000000,
+    },
+    server: {
+      hmr: process.env.DISABLE_HMR !== 'true',
+      watch: process.env.DISABLE_HMR === 'true' ? null : {},
+    },
+  };
+});
